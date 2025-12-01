d88b00923cd3f3f15feed187d4aa27b07b18d272
MyNameIs-13 <g.s@writeme.com>
2025-12-01 18:53:47 +0100

    Initialize Advent of Code 2025

---
+++ README.md
@@ -0,0 +1,8 @@
+# Advent of Code 2025
+
+This repo contains my solutions for Advent of Code - https://adventofcode.com/
+
+## Interpreter
+
+- This project uses shared venv at: /home/skodo/documents/scm/aoc_python/.venv
+- Open this folder in PyCharm, then select the shared interpreter if symlink has not been created.
---
+++ aoc/run_or_create_day.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+import importlib
+from shared.logger import logger
+from shared.create_new_day import create_day
+from shared.utils import get_day
+# import logging
+# logger.setLevel(logging.DEBUG)
+
+
+def __run_or_create_day(day: int):
+    """
+    When the file for the day exists, run it
+    Otherwise create the file from the template
+    """
+    module_name = f'aoc.days.day{day:02}'
+    try:
+        module = importlib.import_module(module_name)
+        module.main()
+    except ModuleNotFoundError:
+        logger.warning(f'❌ Day {day:02} not found. Creating from template...')
+        create_day(day)
+        module = importlib.import_module(module_name)
+        module.main()
+
+
+if __name__ == '__main__':
+    day_num = None
+    # day_num = 1  # day overwrite
+    day_num = get_day(sys.argv[1:], day_num)
+    if day_num:
+        __run_or_create_day(day_num)
